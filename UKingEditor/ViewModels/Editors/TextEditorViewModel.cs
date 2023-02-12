60353edbd34562adbb98e3009fbf545244475b5e
ArchLeaders <archparliament@gmail.com>
2023-02-11 18:13:24 -0800

    Add TextEditor... editor

---
+++ UKingEditor/ViewModels/Editors/TextEditorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Dock.Model.ReactiveUI.Controls;
+using UKingEditor.Core.EditorInterfaces;
+
+namespace UKingEditor.ViewModels.Editors;
+
+public class TextEditorViewModel : Document
+{
+    private readonly ITextEditor _editor;
+
+	public TextEditorViewModel(ITextEditor editor)
+	{
+        Title = "EDITOR";
+        Id = "Idk";
+
+        _editor = editor;
+	}
+}
---
+++ UKingEditor/Views/Editors/TextEditorView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace UKingEditor.Views.Editors
+{
+    public partial class TextEditorView : UserControl
+    {
+        public TextEditorView()
+        {
+            InitializeComponent();
+        }
+    }
+}
